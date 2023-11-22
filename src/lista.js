3e90f04c85f2888e0985888532732bc62945ef0b
Joao Paulo <fernandes.joao@estudante.ufjf.br>
2023-11-21 22:59:29 -0300

    Cria funções

---
+++ src/lista.js
@@ -0,0 +1,23 @@
+const lista = [
+    "Um",
+    "Dois",
+    "Três"
+];
+
+function getLista(){
+    return lista;
+}
+
+function adicionaNaLista(){
+    lista.push(item);
+}
+
+function removeDaLista(){
+    lista.splice(posicao, 1);
+}
+
+function limpaLista(){
+    lista.splice(0); 
+}
+
+export {getLista, adicionaNaLista, removeDaLista, limpaLista};
