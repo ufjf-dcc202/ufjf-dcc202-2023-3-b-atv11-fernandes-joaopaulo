b18a93d02c9dbcb07cd2bbbc9d7d8fc852b1fd00
Joao Paulo <fernandes.joao@estudante.ufjf.br>
2023-11-21 22:59:40 -0300

    Adiciona item no estado

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
+import { adicionaNaLista, getLista } from "./src/lista.js";
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#Limpar");
 const olItens = document.querySelector("#itens");
 
-adicionaELementoNaLista("Primeiro");
-adicionaELementoNaLista("Segundo");
-adicionaELementoNaLista("Terceiro");
+atualizaListaOrdenada();
+btnAdicionar.addEventListener('click', adicionaItemNaEntrada);
+
+function adicionaItemNaEntrada(){   
+    const valor = pEntrada.textContent;
+    adicionaElementoNaLista(valor);
+    pEntrada.textContent = "";
+    atualizaListaOrdenada();
+}
+
+function atualizaListaOrdenada(){
+    const lista = getLista();
+    olItens.innerHTML = "";
+    for(let i=0; i < lista.length; i++){
+        adicionaElementoNaLista(lista[i]);
+    }
+}
 
 
-function adicionaELementoNaLista(texto){
+function adicionaElementoNaLista(texto){
     const li = document.createElement("li");
     li.textContent = texto;
     olItens.appendChild(li);
